445f82a2fc3767b612d9ecec6892062fdba6bf18
Christopher Boyle <cboyle824@gmail.com>
2021-09-10 13:58:38 -0400

    task get endpoint and part of the model pass

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,18 @@
-// build your `Task` model here
+const db = require("./../../data/dbConfig");
+
+const getAll = () => {
+  return db("tasks as t")
+    .leftJoin("projects as p", "p.project_id", "t.project_id")
+    .select(
+      "task_id",
+      "task_description",
+      "task_notes",
+      "task_completed",
+      "project_name",
+      "project_description"
+    );
+};
+
+module.exports = {
+  getAll,
+};
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,21 @@
-// build your `/api/tasks` router here
+const express = require("express");
+const Tasks = require("./model");
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const tasks = await Tasks.getAll();
+    const convertedTasks = tasks.map((task) => {
+      return {
+        ...task,
+        task_completed: Boolean(task.task_completed),
+      };
+    });
+    res.status(200).json(convertedTasks);
+  } catch (err) {
+    next();
+  }
+});
+
+module.exports = router;
